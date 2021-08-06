adacf616c9e0418d28a027624e5c39317b08abb4
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-08-06 19:01:33 +0300

    Add index method logic to retrieve all pizza types

---
+++ app/Controllers/Http/PizzaTypesController.ts
@@ -0,0 +1,48 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import PizzaType from 'App/Models/PizzaType';
+import StorePizza from 'App/Validators/StorePizzaValidator';
+
+export default class PizzaTypesController {
+    public async index({}: HttpContextContract) {
+        const pizzaTypes = await PizzaType.all();
+
+        return {
+            status: 'success',
+            data: pizzaTypes,
+            message: 'Pizzas retrieved successfully'
+        }
+    }
+
+    public async create({}: HttpContextContract) {}
+
+    public async store({ request, response }: HttpContextContract) {
+        const pizzaType = new PizzaType();
+        const payload = await request.validate(StorePizza);
+
+        pizzaType.name = payload.name;
+        pizzaType.price = payload.price;
+        await pizzaType.save();
+        response.status(201);
+
+        if (!pizzaType.$isPersisted){
+            return {
+                status: 'fail',
+                message: 'Failed to create new pizza'
+            }
+        }
+
+        return {
+            status: 'success',
+            data: pizzaType, 
+            message: 'Pizza created successfully'
+        }
+    }
+
+    public async show({}: HttpContextContract) {}
+
+    public async edit({}: HttpContextContract) {}
+
+    public async update({}: HttpContextContract) {}
+
+    public async destroy({}: HttpContextContract) {}
+}
