291951514cd4471e6c42ede9a0f8e6390cc57f82
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-08-07 00:23:22 +0300

    Define relationships on order and product models

--- app/Models/Product.ts
+++ app/Models/Product.ts
@@ -1,9 +1,10 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon';
+import { BaseModel, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm';
+import Order from 'App/Models/Order';
 
 export default class Product extends BaseModel {
     @column({ isPrimary: true })
-    public id: number
+    public id: number;
 
     @column()
     public name: string;
@@ -12,8 +13,11 @@ export default class Product extends BaseModel {
     public price: number;
 
     @column.dateTime({ autoCreate: true })
-    public createdAt: DateTime
+    public createdAt: DateTime;
 
     @column.dateTime({ autoCreate: true, autoUpdate: true })
-    public updatedAt: DateTime
+    public updatedAt: DateTime;
+
+    @hasMany(() => Order)
+    public orders: HasMany<typeof Order>;
 }
