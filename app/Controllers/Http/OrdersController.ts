62af2df18b3f9dea9b232eb88f1e7f7da3fac4bf
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-08-07 10:08:17 +0300

    Add functionality to display an order with its attributes

--- app/Controllers/Http/OrdersController.ts
+++ app/Controllers/Http/OrdersController.ts
@@ -1,17 +1,29 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import Database from '@ioc:Adonis/Lucid/Database';
 import Order from 'App/Models/Order';
 import CreateOrder from 'App/Validators/CreateOrderValidator';
 
 export default class OrdersController {
     public async index({}: HttpContextContract) {
-        const orders = await Order.query().preload('product');
+        const orders = await Database.from('orders')
+            .join('products', 'orders.product_id', '=', 'products.id')
+            .select('orders.id')
+            .select('orders.quantity')
+            .select('products.name')
+            .select('products.price')
+
+        if (Object.keys(orders).length === 0) {
+            return {
+                status: 'fail',
+                message: 'Failed to retrieve orders'
+            }
+        }
 
         return {
             status: 'success',
             data: orders,
             message: 'Orders retrieved successfully'
         }
-
     }
 
     public async create({}: HttpContextContract) {}
@@ -39,7 +51,28 @@ export default class OrdersController {
         }
     }
 
-    public async show({}: HttpContextContract) {}
+    public async show({ params }: HttpContextContract) {
+        const order = await Database.from('orders')
+            .join('products', 'orders.product_id', '=', 'products.id')
+            .select('orders.id')
+            .select('orders.quantity')
+            .select('products.name')
+            .select('products.price')
+            .where('orders.id', params.id)
+
+        if (Object.keys(order).length === 0) {
+            return {
+                status: 'fail',
+                message: 'Failed to retrieve order whose id is ' + params.id
+            }
+        }
+
+        return {
+            status: 'success',
+            data: order,
+            message: 'Order retrieved successfully'
+        }
+    }
 
     public async edit({}: HttpContextContract) {}
 
